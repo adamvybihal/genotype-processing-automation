ee24f08e4aeeebd5892eabb7ba794493e88d061e
Adam Vybíhal <xvybih05@vutbr.cz>
2021-02-12 22:25:31 +0100

    references repair

--- GenotypeDataProcessing/GenotypeDataProcessing/FormDataParams.cs
+++ GenotypeDataProcessing/GenotypeDataProcessing/FormDataParams.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using GenotypeDataProcessing.Structures;
-using GenotypeDataProcessing.ParamFiles;
+using GenotypeDataProcessing.ParamFilesCreation;
 
 namespace GenotypeDataProcessing
 {
